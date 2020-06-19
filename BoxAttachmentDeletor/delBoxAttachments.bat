d19a196aecb32e52c725301a925790dad60e45c2
Rushabh Jhaveri <rjhaveri98@gmail.com>
2020-06-19 10:28:49 -0400

    edited script to generalize download location

--- BoxAttachmentDeletor/delBoxAttachments.bat
+++ BoxAttachmentDeletor/delBoxAttachments.bat
@@ -1,7 +1,7 @@
 echo Delete downloaded email attachments which have since been uploaded to Box. 
 
-del "D:\Downloads\*rrj*.png" /s /f 
+del "%USERPROFILE%\Downloads\*rrj*.png" /s /f 
 
-del "D:\Downloads\*rrj*.jpg" /s /f 
+del "%USERPROFILE%\Downloads\*rrj*.jpg" /s /f 
 
 echo Done! 
