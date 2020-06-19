cd67b187f1f16cba90e9fad8913f053e11e368eb
Rushabh Jhaveri <rjhaveri98@gmail.com>
2020-06-19 13:39:26 -0400

    generalized netID formats.

--- BoxAttachmentDeletor/delBoxAttachments.bat
+++ BoxAttachmentDeletor/delBoxAttachments.bat
@@ -1,7 +1,12 @@
+@ECHO OFF
+
 echo Delete downloaded email attachments which have since been uploaded to Box. 
+SET "sourcedir=%USERPROFILE%\Downloads"
 
-del "%USERPROFILE%\Downloads\*rrj*.png" /s /f 
-
-del "%USERPROFILE%\Downloads\*rrj*.jpg" /s /f 
+for /f "delims=" %%i in ('dir /b /s /a-d %sourcedir% ^| findstr /i /r /c:"^.*-[A-Za-z][A-Za-z][0-9][0-9][0-9][0-9]-.*.jpg$" 
+/c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9][0-9]-.*.jpg$" /c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9][0-9][0-9]-.*.jpg$" /c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9]-.*.jpg$"
+/c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9][0-9]-.*.jpg$" 
+/c:"^.*-[A-Za-z][A-Za-z][0-9][0-9][0-9][0-9]-.*.png$" /c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9][0-9]-.*.png$" /c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9][0-9][0-9]-.*.png$" 
+/c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9]-.*.png$" /c:"^.*-[A-Za-z][A-Za-z][A-Za-z][0-9][0-9]-.*.png$"') do del "%%i"
 
 echo Done! 
