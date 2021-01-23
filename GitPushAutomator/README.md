5b7a121f748eabde10f723f6e896a56941c19a43
Rushabh Jhaveri <rjhaveri98@gmail.com>
2021-01-23 17:09:33 -0500

    push

---
+++ GitPushAutomator/README.md
@@ -0,0 +1,1 @@
+Automating the git add-commit-push deployment. 
---
+++ GitPushAutomator/gitpush.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+# Simple utility script to automate git add-commit-push 
+
+# Todo: add functionality to commit specific files 
+git add . 
+
+echo "Commit message:" 
+
+read commit_message 
+
+git commit -m $commit_message 
+
+echo "Commit Branch:" 
+
+read branch 
+
+git push origin $branch 
+
+read 
