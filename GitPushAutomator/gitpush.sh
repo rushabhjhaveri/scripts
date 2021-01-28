d86531caafade18d6b8bc41580dc7015f494bedf
Rushabh Jhaveri <rjhaveri98@gmail.com>
2021-01-28 14:12:57 -0500

    added comments

--- GitPushAutomator/gitpush.sh
+++ GitPushAutomator/gitpush.sh
@@ -1,28 +1,32 @@
 #!/bin/bash 
 # Simple utility script to automate git add-commit-push 
 
-
-function addChanges(){
-    # TODO: add functionality to commit specific files 
+# Add untracked changes to be tracked for commit
+# TODO: add functionality to commit specific files
+function addChanges(){ 
     git add .
 }
- 
+
+# Add tracked changes to be committed 
 function commitChanges(){
     echo "Commit message:" 
     
     read commit_message 
     
-    git commit -m "$commit_message" 
+    git commit -m "${commit_message}" 
 }
 
+# Push committed changes 
+# TODO: add functionality to have a default commit branch 
 function pushChanges(){
     echo "Commit Branch:" 
 
     read branch 
 
-    git push origin "$branch" 
+    git push origin "${branch}" 
 } 
 
+# Driver to execute the full add-commit-push cycle 
 function deploy(){
     addChanges 
     commitChanges
